e81ede1c62152b1985e8333bfb121ded0c4bf38a
rossaarani <rossaa2810@gmail.com>
2026-07-23 15:10:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const modeToggle = document.querySelector("#modeToggle");
+
+modeToggle.addEventListener("click", function() {
+    document.body.classList.toggle("light");
+
+    if(document.body.classList.contains("light")){
+        modeToggle.textContent = "mode Terang";
+    } else {
+        modeToggle.textContent = "mode Gelap";
+    }
+})
